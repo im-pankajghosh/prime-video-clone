85aba1ce4839b200d61cdb58296dc026892194d4
PankajGhosh998 <Pankajghosh998@gmail.com>
2023-02-20 20:02:08 +0530

    style : add page body css

--- prime-video-ui/src/App.jsx
+++ prime-video-ui/src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './App.css'
 import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
 import Login from './pages/login'
 import Overview from './pages/Overview'
