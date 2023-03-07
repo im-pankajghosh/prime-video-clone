c06ea416f12064490d0e0a10a89e66d5da03bf41
PankajGhosh998 <Pankajghosh998@gmail.com>
2023-03-07 12:27:41 +0530

    docs : add multiple pages route

--- prime-video-ui/src/App.jsx
+++ prime-video-ui/src/App.jsx
@@ -1,20 +1,22 @@
-import React from 'react'
-import './App.css'
-import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
-import Login from './pages/Login'
-import Overview from './pages/Overview'
-import Primevideo from './pages/Primevideo'
-import Signup from './pages/Signup'
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Overview from "./pages/Overview";
+import Primevideo from "./pages/Primevideo";
+import Error from "./pages/Error";
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/signup" element={<Signup/>}/>
-        <Route path="/overview" element={<Overview/>}/>
-        <Route path="/" element={<Primevideo/>}/>
+        <Route path="/overview" element={<Overview />} />
+        <Route path="/signin" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/" element={<Primevideo />} />
+        <Route path="*" element={<Error />} />
       </Routes>
     </Router>
-  )
+  );
 }
