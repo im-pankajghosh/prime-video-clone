8e5da8bf713497e0e7a5154db57b666ec4fad8c5
PankajGhosh998 <Pankajghosh998@gmail.com>
2023-03-13 16:45:45 +0530

    docs : add movie player

---
+++ prime-video-ui/src/pages/Watch.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function Watch() {
+  return (
+    <Container>
+      <div className="player">
+        <video
+          src="https://s3.ll.videorolls.row.aiv-cdn.net/ww_sin/aa6b/9431/3dee/4abd-997d-bbe7101a55c0/8eac6d78-ca60-4de1-a200-95f325f23385_video_720p_4000kbps_audio_aaclc_128kbps.mp4"
+          autoPlay
+          loop
+          muted
+          controls
+        />
+      </div>
+    </Container>
+  );
+}
+const Container = styled.div`
+  .player{
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    min-height: 100vh;
+    height: 100%;
+    box-sizing: border-box;
+    video{
+      object-fit: cover;
+      width: 100%;
+      height: 100%;
+    }
+  }
+`;
