0faf8dd457848e68c61955198c6bfeb54b25a2dd
PankajGhosh998 <Pankajghosh998@gmail.com>
2023-03-14 15:21:53 +0530

    docs : remove few things

--- prime-video-ui/src/pages/Watch.jsx
+++ prime-video-ui/src/pages/Watch.jsx
@@ -8,8 +8,6 @@ export default function Watch() {
         <video
           src="https://s3.ll.videorolls.row.aiv-cdn.net/ww_sin/aa6b/9431/3dee/4abd-997d-bbe7101a55c0/8eac6d78-ca60-4de1-a200-95f325f23385_video_720p_4000kbps_audio_aaclc_128kbps.mp4"
           autoPlay
-          loop
-          muted
           controls
         />
       </div>
@@ -17,18 +15,28 @@ export default function Watch() {
   );
 }
 const Container = styled.div`
-  .player{
+  .player {
     position: absolute;
+    display: flex;
+    background-color: #000;
     top: 0;
     left: 0;
     right: 0;
     min-height: 100vh;
     height: 100%;
     box-sizing: border-box;
-    video{
+    align-items: center;
+    video {
       object-fit: cover;
       width: 100%;
-      height: 100%;
+    }
+  }
+
+  @media screen and (min-width: 1023px) {
+    .player {
+      video {
+        height: 100%;        
+      }
     }
   }
 `;
