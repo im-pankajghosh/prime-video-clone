c52598940854959af23dd67e54ce63cacfa2e1a7
PankajGhosh998 <Pankajghosh998@gmail.com>
2023-03-15 12:22:06 +0530

    docs : remove & add page

--- prime-video-ui/src/App.jsx
+++ prime-video-ui/src/App.jsx
@@ -3,23 +3,24 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
-import Overview from "./pages/Overview";
-import Primevideo from "./pages/Primevideo";
 import Error from "./pages/Error";
-// import ProtectedRoute from "./components/ProtectedRoute";
 import Details from "./pages/Details";
 import Watch from "./pages/Watch";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Home from "./components/Home";
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/overview" element={<Overview />} />
         <Route path="/signin" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/" element={<Primevideo />} />
-        <Route path="/details" element={<Details />} />
-        <Route path="/watch" element={<Watch />} />
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/details"
+          element={<ProtectedRoute component={Details} />}
+        />
+        <Route path="/watch" element={<ProtectedRoute component={Watch} />} />
         <Route path="*" element={<Error />} />
       </Routes>
     </Router>
---
+++ prime-video-ui/src/components/ProtectedRoute.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function ProtectedRoute(props) {
+  const { Component } = props;
+  const navigate = useNavigate();
+  useEffect(() => {
+    let login = localStorage.getItem("login");
+    if (!login) {
+        navigate("/");
+    }
+  });
+  return (
+    <div>
+      <Component />
+    </div>
+  );
+}
